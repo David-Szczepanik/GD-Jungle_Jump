d891b006a01718fbac4d55c55a3ed119030762a2
David-Szczepanik <Davidszczepanik@seznam.cz>
2024-08-22 21:51:31 +0200

    level01

--- levels/level.gd
+++ levels/level.gd
@@ -12,14 +12,14 @@ func _ready():
 
 func set_camera_limits():
 	var map_size = $World.get_used_rect()
-	var cell_size = $World.title_set.title_size
+	var cell_size = $World.tile_set.tile_size
 	$Player/Camera2D.limit_left = (map_size.position.x - 5) * cell_size.x
 	$Player/Camera2D.limit_right = (map_size.end.x + 5) * cell_size.x
 
 func spawn_items():
 	var item_cells = $Items.get_used_cells(0)
 	for cell in item_cells:
-		var data = $Items.get_cell_title_data(0, cell)
+		var data = $Items.get_cell_tile_data(0, cell)
 		var type = data.get_custom_data("type")
 		var item = item_scene.instantiate()
 		add_child(item)
